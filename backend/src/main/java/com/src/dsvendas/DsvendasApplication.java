555b7f28c47b22832e3971893657f68046516f33
Daniel <danielsaraldi@gmail.com>
2021-05-03 21:09:04 -0300

    feat: create work space backend

---
+++ backend/src/main/java/com/src/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.src.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/src/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
