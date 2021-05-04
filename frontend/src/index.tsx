873a35a9659c44557c06da4e1572958d1b6e9add
Daniel <danielsaraldi@gmail.com>
2021-05-04 20:24:29 -0300

    styles: change names from global styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import "assets/css/styles.css";
+import "./assets/styles/global.css";
 import App from "./App";
 
 ReactDOM.render(
