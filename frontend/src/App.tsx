54ff242d4a4393116cebe325eacbf192e023f8df
Daniel <danielsaraldi@gmail.com>
2021-05-04 21:16:53 -0300

    feat: create BarChart component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -7,7 +8,23 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <div className="text-primary">Hello World!</div>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <BarChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />
