dd21c56e4e859dc758a4e46b99adbdcf04de3014
Daniel <danielsaraldi@gmail.com>
2021-05-05 21:23:04 -0300

    feat: create entity Seller

---
+++ backend/src/main/java/com/src/dsvendas/entities/Seller.java
@@ -0,0 +1,52 @@
+package com.src.dsvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincrement in ID
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller") // One Seller to Many Sales and mapped by variable seller
+	private List<Sale> sales = new ArrayList<>();
+	
+	public Seller() {
+	}
+
+	public Seller(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+}
