c0f3f88062f6746cf4cbbecae75e412c2391394b
Daniel <danielsaraldi@gmail.com>
2021-05-06 21:56:20 -0300

    feat: add query amount by Seller in SaleController

--- backend/src/main/java/com/src/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/src/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.src.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.src.dsvendas.dto.SaleDTO;
+import com.src.dsvendas.dto.SaleSumDTO;
 import com.src.dsvendas.services.SaleService;
 
 @RestController
@@ -22,5 +25,11 @@ public class SaleController {
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
 		Page<SaleDTO> salesDTO = saleService.findAll(pageable);
 		return ResponseEntity.ok(salesDTO);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGruopedBySeller() {
+		List<SaleSumDTO> salesSumDTO = saleService.amountGruopedBySeller();
+		return ResponseEntity.ok(salesSumDTO);
 	}
 }
