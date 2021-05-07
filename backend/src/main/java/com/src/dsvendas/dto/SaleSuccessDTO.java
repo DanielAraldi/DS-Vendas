80116503ba31076898ba0e06d1e343647099b88d
Daniel <danielsaraldi@gmail.com>
2021-05-07 20:02:49 -0300

    feat: create SaleSuccessDTO

---
+++ backend/src/main/java/com/src/dsvendas/dto/SaleSuccessDTO.java
@@ -0,0 +1,52 @@
+package com.src.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.src.dsvendas.entities.Seller;
+
+public class SaleSuccessDTO implements Serializable {
+	
+	/**
+	Ensures that data can be converted to bytes, 
+	ensuring that your object can be trafficked 
+	over the network and downloaded to files
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String sellerName;
+	private Long visited;
+	private Long deals;
+	
+	public SaleSuccessDTO() {
+	}
+
+	public SaleSuccessDTO(Seller seller, Long visited, Long deals) {
+		sellerName = seller.getName();
+		this.visited = visited;
+		this.deals = deals;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Long getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Long visited) {
+		this.visited = visited;
+	}
+
+	public Long getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Long deals) {
+		this.deals = deals;
+	}
+}
