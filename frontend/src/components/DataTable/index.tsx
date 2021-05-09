97c2c3b5b5ece7a4bd45d394f49cb3c508bd506b
Daniel <danielsaraldi@gmail.com>
2021-05-08 23:19:04 -0300

    feat: DataTable integration done

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -6,7 +6,10 @@ import { SalePage } from "@types";
 
 import { formatDate } from "utils";
 
+import Pagination from "components/Pagination";
+
 function DataTable() {
+  const [numberPage, setNumberPage] = useState(0);
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -17,35 +20,42 @@ function DataTable() {
 
   useEffect(() => {
     api
-      .get("/sales?page=0&size=15&sort=date,desc")
+      .get(`/sales?page=${numberPage}&size=15&sort=date,desc`)
       .then((response) => setPage(response.data));
-  }, []);
+  }, [numberPage]);
+
+  const changePage = (numberPage: number) => setNumberPage(numberPage);
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm mb-5">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {page.content?.map(({ id, amount, date, deals, seller, visited }) => (
-            <tr key={id}>
-              <td>{formatDate(date, "dd/MM/yyyy")}</td>
-              <td>{seller.name}</td>
-              <td>{visited}</td>
-              <td>{deals}</td>
-              <td>{amount.toFixed(2)}</td>
+    <>
+      <Pagination page={page} onPageChange={changePage} />
+      <div className="table-responsive">
+        <table className="table table-striped table-sm mb-5">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {page.content?.map(
+              ({ id, amount, date, deals, seller, visited }) => (
+                <tr key={id}>
+                  <td>{formatDate(date, "dd/MM/yyyy")}</td>
+                  <td>{seller.name}</td>
+                  <td>{visited}</td>
+                  <td>{deals}</td>
+                  <td>{amount.toFixed(2)}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
