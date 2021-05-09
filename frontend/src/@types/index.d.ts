b9b8efadaac20e518b73693453d6509cc69d48c0
Daniel <danielsaraldi@gmail.com>
2021-05-08 21:54:18 -0300

    feat: create BarChartData types

--- frontend/src/@types/index.d.ts
+++ frontend/src/@types/index.d.ts
@@ -1,9 +1,29 @@
-export interface ChartData {
+export interface DonutChartData {
   series: number[];
   labels: string[];
+}
+
+interface Categories {
+  categories: string[];
+}
+
+interface SeriesData {
+  name: string;
+  data: number[];
+}
+
+export interface BarChartData {
+  labels: Categories;
+  series: SeriesData[];
 }
 
 export interface SaleSum {
   sellerName: string;
   sum: number;
+}
+
+export interface SaleSuccess {
+  sellerName: string;
+  visited: number;
+  deals: number;
 }
