d5011bee5e6f991b7084b6a8efcdaacce81b818f
Daniel <danielsaraldi@gmail.com>
2021-05-08 23:31:02 -0300

    feat: add enviroment variable production

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const URL =
+  process.env.REACT_APP_URL_PRODUCTION ?? process.env.REACT_APP_BASE_URL;
+
 export const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}`,
+  baseURL: `${URL}`,
 });
